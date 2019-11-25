ea8e09ed7e8fed09e6720b27464228ba1760e267
Constantin Eulenstein <constantin.eulenstein@t-online.de>
2019-11-25 16:56:27 +0800

    Started setting up validations for the three models - presence of values and uniqueness of pairings for trademark and for transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :trademark
   belongs_to :user
+  validates :location, :date, :description, :user_id, :trademark_id, presence: true
+  validates :user_id, uniqueness: { scope: :trademark_id}
 end
