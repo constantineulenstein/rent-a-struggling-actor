56c54c88f2926bac7e1f0c7c5a6cca2639f574a5
Daniel Galimberti <daniel.galimberti@ecfo.asia>
2019-11-26 13:21:34 +0800

    Seeds updated, 1 known user, 10 regular users, 20 actors with 2 skills each

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,28 +7,48 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
+Trademark.destroy_all
 
-30.times do
-  User.create(email: Faker::Internet.email,
+User.create(email: "leonardo@gmail.com",
+            password: "123456",
+            first_name: "Leonardo",
+            last_name: "Dicaprio",
+            username: "Leonardo69",
+)
+
+
+
+10.times do
+  user = User.create(email: Faker::Internet.email,
+              password: "123456",
+              first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              username: Faker::Name.middle_name,
+
+              )
+  end
+
+
+20.times do
+  user = User.create(email: Faker::Internet.email,
               password: "123456",
               first_name: Faker::Name.first_name,
               last_name: Faker::Name.last_name,
               username: Faker::Name.middle_name,
               gender: Faker::Gender.binary_type,
-              age: rand(18..80).to_s,
+              age: rand(18..80),
               location: Faker::Address.city,
               description: Faker::Verb.base,
-              actor: [true, false].sample
+              actor: true
               )
 
+
+  2.times do
+    Trademark.create(skill: Faker::Games::ElderScrolls.creature, user_id: user.id)
+  end
 end
 
 
-Trademark.destroy_all
-
-30.times do
-  Trademark.create(skill: Faker::Games::ElderScrolls.creature, user: User.all.sample)
-end
 
 puts "created #{User.count} users"
 puts "created #{Trademark.count} trademarks"
