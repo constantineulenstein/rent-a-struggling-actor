9620f8785ce828891dd904819c270a3607332f2f
Elliot Tennison <elliot.s.tennison@gmail.com>
2019-11-26 13:13:44 +0800

    added controllers for users and transactions as well as routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,10 @@ class UsersController < ApplicationController
   before_action :find_user, only: [:show, :edit, :update]
 
   def index
-    @users = User.all #where(["actor = ?", true]) # filter searches for actor here?
+    @users = User.where(["actor = ?", true])
   end
 
   def show
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @restaurant = User.new(user_params)
-    if @user.save
-      redirect_to @user
-    else
-      render :new
-    end
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:index,:show,:new,:create,:edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resources :transactions, only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
