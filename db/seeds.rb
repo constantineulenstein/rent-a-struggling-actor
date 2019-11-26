8bfecb10de6349647ed46e5c55d0460c3fd544a6
Daniel Galimberti <daniel.galimberti@ecfo.asia>
2019-11-26 12:23:48 +0800

    Seed Files Added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+30.times do
+  User.create(email: Faker::Internet.email,
+              password: "123456",
+              first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              username: Faker::Name.middle_name,
+              gender: Faker::Gender.binary_type,
+              age: rand(18..80).to_s,
+              location: Faker::Address.city,
+              description: Faker::Verb.base,
+              actor: [true, false].sample
+              )
+
+end
+
+
+Trademark.destroy_all
+
+30.times do
+  Trademark.create(skill: Faker::Games::ElderScrolls.creature, user: User.all.sample)
+end
+
+puts "created #{User.count} users"
+puts "created #{Trademark.count} trademarks"
