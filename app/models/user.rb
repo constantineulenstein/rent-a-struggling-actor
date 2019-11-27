8334b1a621f0fdcf9d8c3b964289b4b0d9c49258
Constantin Eulenstein <constantin.eulenstein@t-online.de>
2019-11-27 14:21:14 +0800

    Updated seed file, included photos to be uploaded to cloud

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   mount_uploader :photo, PhotoUploader
-  has_many :transactions
-  has_many :trademarks
+  has_many :transactions, dependent: :destroy
+  has_many :trademarks, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
