1a0afce4347fe3ca79e586a5bb1786ba9a93ab10
Elliot Tennison <elliot.s.tennison@gmail.com>
2019-11-27 13:35:40 +0800

    added styling to show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :actor])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :actor, :age, :gender, :location, :description, :photo, :trademark])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :actor, :age, :gender, :location, :description, :photo])
   end
 
   private
