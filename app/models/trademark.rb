d31f0b149a5eb015ab1fcfe143e2ee1ceec07563
Constantin Eulenstein <constantin.eulenstein@t-online.de>
2019-11-29 12:57:54 +0800

    Added dependent destroy on trademarks and transacvtions

--- app/models/trademark.rb
+++ app/models/trademark.rb
@@ -1,6 +1,6 @@
 class Trademark < ApplicationRecord
   belongs_to :user
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
   validates :skill, presence: true
   validates :skill, uniqueness: { scope: :user_id}
 
